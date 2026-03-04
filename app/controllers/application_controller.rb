9db770e0f3e9327d426facf81f0f73d05e5117f8
Abner Dascalakis <abnerdascalakis@gmail.com>
2026-03-03 20:13:16 -0400

    card login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_locale
   
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = :"pt-BR"
   end
 end
